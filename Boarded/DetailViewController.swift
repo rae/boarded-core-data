6290c1a74ffe51d8c9807f7610d1dc8d49f17bf7
Reid Ellis <rae@tnir.org>
2017-01-10 17:48:01 -0500

    Initial Commit

---
+++ Boarded/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Boarded
+//
+//  Created by Reid Ellis on 2017-01-10.
+//  Copyright © 2017 Tnir Technologies. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+	func configureView() {
+		// Update the user interface for the detail item.
+		if let detail = self.detailItem {
+		    if let label = self.detailDescriptionLabel {
+		        label.text = detail.timestamp!.description
+		    }
+		}
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.configureView()
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	var detailItem: Event? {
+		didSet {
+		    // Update the view.
+		    self.configureView()
+		}
+	}
+
+
+}
+
